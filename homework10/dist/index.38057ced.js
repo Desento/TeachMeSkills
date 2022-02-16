d84ff472f9678b3a2d868f986f8364ce5ad419c6
Desento <astapenko91@gmail.com>
2022-02-16 19:24:11 +0300

    end homework

--- homework10/dist/index.38057ced.js
+++ homework10/dist/index.38057ced.js
@@ -541,16 +541,11 @@ const swiper = new _swiperDefault.default('.customers-slider', {
     },
     breakpoints: {
         // when window width is <= 320px
-        320: {
+        576: {
             slidesPerView: 1,
-            spaceBetween: 10
+            spaceBetween: 30
         },
         // when window width is <= 480px
-        480: {
-            slidesPerView: 2,
-            spaceBetween: 20
-        },
-        // when window width is <= 640px
         640: {
             slidesPerView: 3,
             spaceBetween: 30
