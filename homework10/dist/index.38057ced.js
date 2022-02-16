85e30ce2afd17713bb78c6bdbacc5150efc87659
Desento <astapenko91@gmail.com>
2022-02-16 16:54:24 +0300

    customers reddy

--- homework10/dist/index.38057ced.js
+++ homework10/dist/index.38057ced.js
@@ -538,6 +538,23 @@ const swiper = new _swiperDefault.default('.customers-slider', {
     // And if we need scrollbar
     scrollbar: {
         el: '.swiper-scrollbar'
+    },
+    breakpoints: {
+        // when window width is <= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 10
+        },
+        // when window width is <= 480px
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        // when window width is <= 640px
+        640: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        }
     }
 });
 
--- homework10/src/main.js
+++ homework10/src/main.js
@@ -22,4 +22,21 @@ const swiper = new Swiper('.customers-slider', {
     scrollbar: {
       el: '.swiper-scrollbar',
     },
+    breakpoints: {
+        // when window width is <= 320px
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 10
+        },
+        // when window width is <= 480px
+        480: {
+          slidesPerView: 2,
+          spaceBetween: 20
+        },
+        // when window width is <= 640px
+        640: {
+          slidesPerView: 3,
+          spaceBetween: 30
+        }
+      }
   });
