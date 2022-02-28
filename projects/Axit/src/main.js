cfafdcb38594652c90866e40dee6e114c5a52300
Desento <astapenko91@gmail.com>
2022-02-28 20:23:02 +0300

    organized projects

---
+++ projects/Axit/src/main.js
@@ -0,0 +1,39 @@
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+
+const swiper = new Swiper('.customers-slider', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    breakpoints: {
+        500: {
+          slidesPerView: 1,
+          spaceBetween: 30
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 30
+        },
+        992: {
+          slidesPerView: 3,
+          spaceBetween: 30
+        }
+      }
+  });
+
