a8f503c9dd474185fbf53b70872dafc159e93584
Desento <astapenko91@gmail.com>
2022-03-05 14:30:12 +0300

    feat: add index.js task#1-7

---
+++ JS/lesson-16/index.js
@@ -0,0 +1,74 @@
+'use strict'
+
+// let admin;
+// let userName = 'Jon';
+// admin = userName;
+// alert(admin);
+
+// let ourPlanet;
+// let userName;
+
+// let x = 20;
+// let y = 58;
+// let z = 42;
+// let rezult = x+y+z;
+// console.log(rezult);
+
+// let sec = 60;
+// let minute = 60;
+// let hour = 24;
+// let day = 365;
+// let myAge = prompt('Vvedite Vash vozrast');
+// myAge = Number(myAge);
+// let myAgeInSeconds = myAge*day*hour*minute*sec;
+// alert(`Vash vozrast v secundah - ${myAgeInSeconds}`);
+
+// let count = 42;
+// let userName = '42';
+// count = String(count);
+// userName = Number(userName);
+// console.log(count,userName);
+
+// let a = 1;
+// let b = 2;
+// let c = "белых медведей";
+// alert(`${a}${b} ${c}`);
+
+// let a = 'доступ';
+// let b = 'морпех';
+// let c = 'наледь';
+// let d = 'попрек';
+// let f = 'рубило';
+// let lengthWords = a.length+b.length+c.length+d.length+f.length;
+// alert(lengthWords);
+
+// let a = 'доступ';
+// let b = 2;
+// let c = true;
+// let aT = typeof(a);
+// let bT = typeof(b);
+// let cT = typeof(c);
+// alert(`Variable: a have type: ${aT}; Variable: b have type: ${bT}; Variable: c have type: ${cT};`);
+
+// let userName = prompt('Input you Name');
+// let userAge = prompt('Input you age');
+// console.log(userName, userAge);
+
+// let a = 4;
+// let b = 3;
+// a = a + b;
+// b = a - b;
+// a = a - b;
+// alert(`${a} ${b}`);
+
+// let codeWord1 = "обернись";
+// let codeWord2 = "неужели";
+// let codeWord3 = "огурцы";
+// let codeWord4 = "липкие";
+// let codeWord5 = "?!";
+// let first = codeWord1[1];
+// let second = codeWord2[1];
+// let third = codeWord3[1];
+// let foutrh = codeWord4[1];
+// let fifth = codeWord5[1];
+// alert (`${first} ${second} ${third} ${foutrh} ${fifth}`);
