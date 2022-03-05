68d47a9d625ed32d68a0924d395848c0c3670816
Desento <astapenko91@gmail.com>
2022-03-05 16:07:15 +0300

    feat: add index.js extra task#1-5

---
+++ JS/HW-16/index.js
@@ -0,0 +1,44 @@
+'use strict'
+
+// let timeRide = prompt('Input time of ride');
+// timeRide = Number(timeRide);
+// let volumeWater = Math.floor(timeRide * 0.5);
+// alert(`Natan will drink ${volumeWater} litr of water`);
+
+// let year = prompt('Input year');
+// let age = Math.ceil(year / 100);
+// alert(`${year} is ${age} century`);
+
+// let year = prompt('Input year');
+// if (year % 4 == 0) {
+//     alert(`${year} явялется високосным годом`)
+// } else {
+//     alert(`${year} не явялется високосным годом`)
+// }
+
+// let inputPrice = prompt('Input price');
+// inputPrice = Number(inputPrice);
+// inputPrice = inputPrice.toFixed(2);
+// alert(`Price is $${inputPrice}`);
+
+// let operation = prompt('Input operation', ['+ - * /']);
+// let x = prompt('Input first number');
+// let y = prompt('Input second number');
+// x = Number(x);
+// y = Number(y);
+// let z;
+// if (operation === '+') {
+//     z = x + y;
+//     alert(`Result - ${z}`);
+// } else if ( operation === '-' ) {
+//     z = x - y;
+//     alert(`Result - ${z}`);
+// }  else if ( operation === '*' ) {
+//     z = x * y;
+//     alert(`Result - ${z}`);
+// } else if ( operation === '/' ) {
+//     z = x / y;
+//     alert(`Result - ${z}`);
+// } else {
+//     alert(`You input uncorrect operation :(`)
+// }
