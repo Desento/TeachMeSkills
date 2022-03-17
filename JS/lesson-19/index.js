2a0b807996e38fc8480e4bc4b7575b7727f18c39
Desento <astapenko91@gmail.com>
2022-03-17 15:50:58 +0300

    refactor: update code style for index.js [HW-19]

--- JS/lesson-19/index.js
+++ JS/lesson-19/index.js
@@ -4,10 +4,10 @@
 //     John: 100,
 //     Ann: 160,
 //     Pete: 130
-// }
+// };
 // console.log("Ann" in salaries);
 // console.log(salaries);
-// for ( let i in salaries) {
+// for (let i in salaries) {
 //     delete salaries[i];
 // }
 // console.log(salaries);
@@ -16,10 +16,10 @@
 //     name: 'John',
 //     age: 19,
 //     isHappy: true
-// }
-// for ( let i in student) {
-//     console.log(i)
-//     console.log(student[i])
+// };
+// for (let i in student) {
+//     console.log(i);
+//     console.log(student[i]);
 // }
 
 // const colors = {
@@ -41,8 +41,8 @@
 // let sum = 0;
 // let n = 0;
 // for ( let i in salaries) {
-//     sum += salaries[i]
-//     n += 1
+//     sum += salaries[i];
+//     n += 1;
 // }
 // let middle = sum / n;
 
@@ -54,8 +54,8 @@
 // }
 // let prove = confirm(`Are you sure? You login: ${userLogin} and password: ${userPassword}`);
 // if (prove) {
-//     alert('Welcome')
-//     console.log(userInfo)
+//     alert('Welcome');
+//     console.log(userInfo);
 // }
 
 // const formatting = {
@@ -73,10 +73,10 @@
 // let score = prompt('Input score, like 2:5');
 // let key, key2;
 // key = +score[0];
-// key2 = +score[2]
+// key2 = +score[2];
 // score = formatting[key] + score.substring(1);
 // score = score.substring(0, score.length - 1) + formatting[key2];
-// console.log(score)
+// console.log(score);
 
 // let student1 = {
 //     name: 'Polina',
@@ -86,8 +86,7 @@
 //     name: 'Polina',
 //     age: 27,
 // }
-// let result = _.isEqual(student1, student2);
-// console.log(result);
+// console.log(_.isEqual(student1, student2));
 
 // const animals = {
 //     cat: {
@@ -99,4 +98,4 @@
 //        age: 2,
 //     }
 //  }
-//  console.log(animals && animals.bird && animals.bird,name);
+//  console.log(animals && animals.bird && animals.bird.name);
