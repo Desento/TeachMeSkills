3c8db0f845b8a5954b1317f209e4f62feb09aa2a
Anton <97047023+Desento@users.noreply.github.com>
2022-03-21 20:21:04 +0300

    feat: add solution for all tasks [HW-20] (#14)
    * feat: add solution for all tasks [HW-20]
    
    * refactor: update code style for index.js [HW-20]

---
+++ JS/lesson-20/index.js
@@ -0,0 +1,125 @@
+'use strict'
+
+// const styles = ['Jazz', 'Blues'];
+// console.log('Styles', styles);
+// styles.push('Rock&Roll');
+// console.log('Styles', styles);
+// styles[Math.floor(styles.length / 2)] = 'Classic';
+// console.log('Styles', styles);
+// styles.shift();
+// console.log('Styles', styles);
+// styles.unshift('Rap', 'Raggy');
+// console.log('Styles', styles);
+
+// function sumOfNumber(){
+// const numbers = [];
+// while (true) {
+//     let value = prompt("Введите число", 0);
+//     if (value === "" || value === null || !isFinite(value)) break;
+
+//     numbers.push(+value);
+//   }
+// let sum = 0;
+// for (let number of numbers) {
+//     sum += number;
+// }
+// return sum;}
+// alert(sumOfNumber());
+
+// const colors = ['red', 'green', 'blue'];
+// console.log(colors.length);
+
+// const animals = ['monkey', 'dog', 'cat'];
+// console.log(animals[animals.length - 1]);
+// console.log(animals.pop());
+
+// const numbers = [5, 43, 63, 23, 90];
+// let x = numbers.length;
+// const y = 0;
+// numbers.length = y;
+// numbers.length = x;
+// console.log(numbers);
+
+// const numbers2 = [5, 43, 63, 23, 90];
+// for ( let number of numbers) {
+//     numbers2.pop();
+// }
+// console.log(numbers2);
+
+// const students = ['Polina', 'Dasha', 'Masha'];
+// console.log(students);
+// students.pop();
+// students.push('Borya');
+// console.log(students);
+// students.shift();
+// students.unshift('Andrey');
+// console.log(students);
+
+// const cats = ['Gachito', 'Tom', 'Batman'];
+// for (let i = 0; i < cats.length; i += 1){
+//     console.log(cats[i]);
+// };
+// for (let cat of cats) {
+//     console.log(cat);
+// }
+
+// const evenNumbers = [2, 4, 6, 8, 10];
+// const oddNumbers = [1, 3, 5, 7, 9];
+// const numbers = evenNumbers.concat(oddNumbers);
+// console.log(numbers);
+// console.log(numbers.indexOf(8));
+
+// const binary = [0, 0, 0, 0];
+// const full = binary.join('1');
+// console.log(full);
+
+// let input = prompt('Input word');
+// function palindrom (input) {
+//     return Array.from(input).reverse().join('') === input;
+// }
+// alert(palindrom(input));
+
+// const matrix = [
+//     [12, 98, 78, 65, 23],
+//     [54, 76, 98, 43, 65],
+//     [13, 324, 65, 312],
+//     [9092, 22, 45, 90000],
+// ];
+// let sum = 0;
+// let counter = 0; 
+// for ( let i = 0; i < matrix.length; i += 1){
+//     // sum += matrix[i];
+//     for (let j = 0; j < matrix [i].length; j += 1){
+//         sum += matrix[i][j];
+//         counter += 1;
+//         console.log(i, j, sum, counter)
+//     }
+// }
+// console.log(sum / counter);
+
+// const mixedNumbers = [-14, 24, -89, 43, 0 , -1, 412, 4];
+// const positiveNumber = [];
+// const negativeNumber = [];
+// for (let i = 0; i < mixedNumbers.length; i += 1) {
+//     if (mixedNumbers[i] < 0) {
+//         negativeNumber.push(mixedNumbers[i]);
+//     } else {
+//         positiveNumber.push(mixedNumbers[i]);
+//     }
+// }
+// console.log(positiveNumber);
+// console.log(negativeNumber);
+
+// const array = [];
+// const numbersInCub = [];
+// for ( let i = 0; i < 5; i += 1) {
+//     array.push(getRandomInt());
+// }
+// console.log(array);
+// for ( let i = 0; i < array.length; i += 1) {
+//     numbersInCub.push(Math.pow(array[i], 3));
+// }
+// console.log(numbersInCub);
+// function getRandomInt() {
+//     return Math.floor(Math.random() * 100) ;
+// }
