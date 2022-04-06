7d3bc9b819984e34a6f4d08af82e8c9b640d3640
Anton <97047023+Desento@users.noreply.github.com>
2022-04-06 19:23:55 +0300

    feat: add solution for normal level tasks [HW-23] (#16)
    * feat: add insex.js and index.html
    
    * refactor: update nested
    
    * feat: add solution for normal level tasks [HW-23]
    
    * feat: add solution for normal level tasks [HW-24]

---
+++ JS/lesson-23/index.js
@@ -0,0 +1,82 @@
+const root = document.getElementById('root')
+
+// ----------------- Action Panel -----------------
+
+const bigWrapper = document.createElement('div')
+bigWrapper.classList.add('container')
+
+const wrapperRow = document.createElement('div')
+wrapperRow.classList.add('container-wrapper')
+
+const deleteAllButton = document.createElement('button')
+deleteAllButton.textContent = 'Delete All'
+deleteAllButton.classList.add('btn')
+
+const addTodoTextField = document.createElement('input')
+addTodoTextField.placeholder = 'Enter todo...'
+addTodoTextField.setAttribute('id', 'todo-input')
+
+const addTodoButton = document.createElement('button')
+addTodoButton.textContent = 'Add'
+addTodoButton.classList.add('btn')
+
+wrapperRow.append(deleteAllButton, addTodoTextField, addTodoButton)
+
+// ----------------- Todos -----------------
+
+function getTodo(text) {
+  const todoElement = document.createElement('div')
+  todoElement.classList.add('todo-item')
+ todoElement.setAttribute('id', `todo-${Math.floor(Math.random() * 1000) + 1}`);
+
+  const completeButton = document.createElement('button')
+  completeButton.classList.add('btn-todo-action', 'complete')
+  completeButton.innerText = ''
+  completeButton.addEventListener('click', () => {
+      completeButton.innerText = completeButton.innerText === '' ? '✓' : '';
+      todoElement.classList.toggle('complete');
+      todoElement.classList.toggle('activ');
+  })
+
+  const todoTextElement = document.createElement('div')
+  todoTextElement.classList.add('todo-text')
+  todoTextElement.innerHTML = `<span>${text}<span>`
+
+  const columnWrapper = document.createElement('div')
+  columnWrapper.classList.add('column-wrapper')
+  const todoDeleteButton = document.createElement('button')
+  todoDeleteButton.classList.add('btn-todo-action', 'delete')
+  todoDeleteButton.innerText = 'X'
+  const todoDatetimeBox = document.createElement('span')
+  todoDatetimeBox.classList.add('column-wrapper-date')
+  todoDatetimeBox.innerText = (new Date()).toLocaleString()
+  columnWrapper.append(todoDeleteButton, todoDatetimeBox)
+
+  todoElement.append(completeButton, todoTextElement, columnWrapper)
+
+  todoDeleteButton.addEventListener('click', () => todoElement.remove());
+
+  return todoElement;
+}
+
+const createTodo = () => {
+  const text = addTodoTextField.value;
+  bigWrapper.append(getTodo(text));
+  addTodoTextField.value = '';
+}
+const deleteAll = () => {
+    const todoItems = document.querySelectorAll('.todo-item');
+    todoItems.forEach((item) => item.remove());
+
+}
+deleteAllButton.addEventListener('click', deleteAll)
+
+addTodoButton.addEventListener('click', createTodo)
+
+
+// ----------------- Render section -----------------
+
+// let todos = TodoController.generateTodos(2);
+
+bigWrapper.append(wrapperRow)
+root.append(bigWrapper)
