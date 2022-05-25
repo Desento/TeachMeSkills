8dc47bc58bab3e53c8ea12cbbbe34e0eda9b345c
Anton <astapenko91@gmail.com>
2022-05-25 18:36:47 +0300

    feat: add solution for task [HW-34]

--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,4 +1,4 @@
-1. 
+1.
 interface IUser {
     id: number;
     email: string;
@@ -14,7 +14,7 @@ const users: IUser[] = [
         first_name: "Michael",
         last_name: "Lawson",
         avatar: "https://reqres.in/img/faces/7-image.jpg",
-         age: 23
+        age: 23
     },
     {
         id: 8,
@@ -23,7 +23,7 @@ const users: IUser[] = [
         last_name: "Ferguson",
         avatar: "https://reqres.in/img/faces/8-image.jpg",
         age: 20
-     }
+    }
 ];
 
 2.
@@ -46,7 +46,7 @@ const products: product[] = [
         type: "burger",
         available: true
     },
-  {
+    {
         id: 2,
         name: "Burger Lite",
         price: 2.3,
@@ -57,76 +57,101 @@ const products: product[] = [
     },
 ];
 
-3. 
-const getUser = (id: number, users: IUser[]): IUser | undefined => 
-users.find((user: IUser) => user.id === id);
+3.
+const getUser = (id: number, users: IUser[]): IUser | undefined =>
+    users.find((user: IUser) => user.id === id);
 
 
-// const faculties = [
-//     {
-// 		id: 1,
-// 		faculty: "History department",
-// 		subjects: ["The World History", "History of Rome"]
-// 		countStudents: 44
-// 	}, 
-// 	{
-// 		id: 2,
-// 		faculty: "Department of Biology",
-// 		subjects: ["biology", "chemistry"],
-// 		countStudents: 50
-// 	},
-// 	{
-// 		id: 3,
-// 		faculty: "Faculty of Mathematics",
-// 		subjects: ["mathematics", "geometry", "trigonometry"],
-// 		countStudents: 72
-// 	},
-// 	{
-// 		id: 4,
-// 		faculty: "Faculty of Design",
-// 		subjects: ["ui", "ux", "graphic design"],
-// 		countStudents: 37
-// 	}
-// ];
+1.
+interface faculty {
+    id: number;
+    faculty: string;
+    subjects: string[];
+    countStudents: number
+}
+const faculties: faculty[] = [
+    {
+        id: 1,
+        faculty: "History department",
+        subjects: ["The World History", "History of Rome"],
+        countStudents: 44
+    },
+    {
+        id: 2,
+        faculty: "Department of Biology",
+        subjects: ["biology", "chemistry"],
+        countStudents: 50
+    },
+    {
+        id: 3,
+        faculty: "Faculty of Mathematics",
+        subjects: ["mathematics", "geometry", "trigonometry"],
+        countStudents: 72
+    },
+    {
+        id: 4,
+        faculty: "Faculty of Design",
+        subjects: ["ui", "ux", "graphic design"],
+        countStudents: 37
+    }
+];
 
-// const movies = [
-// 				{
-// 						id: 1
-// 						title: "Black Widow",
-// 					  year: 2021,
-// 					  released: "09 Jul 2021",
-// 					  runtime: "134 min",
-// 					  genre: ["Action", "Sci-Fi", "Adventure"],
-// 						director: "Cate Shortland",
-// 					  writer: "Eric Pearson, Jac Schaeffer, Ned Benson",
-// 					  actors: ["Scarlett Johansson", "Florence Pugh", "David Harbour"],
-// 					  plot: "Natasha Romanoff confronts the darker parts of her ledger when a dangerous conspiracy with ties to her past arises.",
-// 					  country: "United States",
-// 					  poster: "https://m.media-amazon.com/images/M/MV5BNjRmNDI5MjMtMmFhZi00YzcwLWI4ZGItMGI2MjI0N2Q3YmIwXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg",
-// 					  imdbRating: 6.9,
-// 					  imdbVotes: 121932,
-// 					  type: "movie",
-// 						boxOffice: "$138,027,361",
-// 					  production: "Marvel Studios",
-//         },
-// 				{
-// 						id: 2,
-// 					  title: "Harry Potter and the Deathly Hallows: Part 2",
-// 					  year: 2011,
-// 					  released: "15 Jul 2011",
-// 					  runtime: "130 min",
-// 					  genre: ["Adventure", "Drama", "Fantasy"],
-// 					  director: "David Yates",
-// 					  writer: "Steve Kloves, J.K. Rowling",
-// 					  actors: ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"],
-// 						plot: "Harry, Ron, and Hermione search for Voldemort's remaining Horcruxes in their effort to destroy the Dark Lord as the final battle rages on at Hogwarts.",
-// 					  country: "United Kingdom, United States",
-// 					  poster: "https://m.media-amazon.com/images/M/MV5BMGVmMWNiMDktYjQ0Mi00MWIxLTk0N2UtN2ZlYTdkN2IzNDNlXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg",
-
-// 					  imdbRating: 8.1,
-// 					  imdbVotes: 790377,
-// 						type: "movie",
-// 					  boxOffice: "$381,409,310",
-// 					  production: "Heyday Films, Moving Picture Company, Warner Bros.",
-// 			},
-// ];
+2.
+interface movi {
+    id: number,
+    title: string,
+    year: number,
+    released: string,
+    runtime: string,
+    genre: string[],
+    director: string,
+    writer: string,
+    actors: string[],
+    plot: string,
+    country: string,
+    poster: string,
+    imdbRating: number,
+    imdbVotes: number,
+    type: string,
+    boxOffice: string,
+    production: string,
+}
+const movies: movi[] = [
+    {
+        id: 1,
+        title: "Black Widow",
+        year: 2021,
+        released: "09 Jul 2021",
+        runtime: "134 min",
+        genre: ["Action", "Sci-Fi", "Adventure"],
+        director: "Cate Shortland",
+        writer: "Eric Pearson, Jac Schaeffer, Ned Benson",
+        actors: ["Scarlett Johansson", "Florence Pugh", "David Harbour"],
+        plot: "Natasha Romanoff confronts the darker parts of her ledger when a dangerous conspiracy with ties to her past arises.",
+        country: "United States",
+        poster: "https://m.media-amazon.com/images/M/MV5BNjRmNDI5MjMtMmFhZi00YzcwLWI4ZGItMGI2MjI0N2Q3YmIwXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg",
+        imdbRating: 6.9,
+        imdbVotes: 121932,
+        type: "movie",
+        boxOffice: "$138,027,361",
+        production: "Marvel Studios",
+    },
+    {
+        id: 2,
+        title: "Harry Potter and the Deathly Hallows: Part 2",
+        year: 2011,
+        released: "15 Jul 2011",
+        runtime: "130 min",
+        genre: ["Adventure", "Drama", "Fantasy"],
+        director: "David Yates",
+        writer: "Steve Kloves, J.K. Rowling",
+        actors: ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"], plot: "Harry, Ron, and Hermione search for Voldemort's remaining Horcruxes in their effort to destroy the Dark Lord as the final battle rages on at Hogwarts.",
+        country: "United Kingdom, United States",
+        poster: "https://m.media-amazon.com/images/M/MV5BMGVmMWNiMDktYjQ0Mi00MWIxLTk0N2UtN2ZlYTdkN2IzNDNlXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg",
+        imdbRating: 8.1,
+        imdbVotes: 790377,
+        type: "movie",
+        boxOffice: "$381,409,310",
+        production: "Heyday Films, Moving Picture Company, Warner Bros.",
+    },
+];
