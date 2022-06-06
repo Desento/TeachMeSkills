12f7ccb2e8471604cfa4d1992ca65bbf64fb9fb3
Anton <astapenko91@gmail.com>
2022-06-06 20:31:48 +0300

    feat: add solution for task Tabs[HW-37]

--- React/my-app/src/App.tsx
+++ React/my-app/src/App.tsx
@@ -1,27 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Hello from './components/Hello/Hello';
+import Tabs from './components/Tabs/Tabs.js';
 
 function App() {
   const text = 'Props text';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p><Hello propsText={text}/></p>
-      </header>
+      <Tabs />
     </div>
   );
 }
